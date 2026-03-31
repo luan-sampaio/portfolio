5995989e0cf3a9442ba519828c9292cb96b8615f
Luan Sampaio <luansampaio0294@gmail.com>
2026-03-31 00:58:26 -0300

    feat: refina textos gerais, adiciona cor interativa às tecnologias e integra projeto libmanager-cli

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("Arquivo main.js conectado com sucesso. Tudo pronto para codar interações!");
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navLinks = document.querySelector('.nav-links');
+    const menuLinks = document.querySelectorAll('.nav-links a');
+
+    mobileMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        
+        const icon = mobileMenu.querySelector('i');
+        if (navLinks.classList.contains('active')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-xmark');
+        } else {
+            icon.classList.remove('fa-xmark');
+            icon.classList.add('fa-bars');
+        }
+    });
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            if (navLinks.classList.contains('active')) {
+                navLinks.classList.remove('active');
+                const icon = mobileMenu.querySelector('i');
+                icon.classList.remove('fa-xmark');
+                icon.classList.add('fa-bars');
+            }
+        });
+    });
 });
